e62f40b6666254e86e2c280f9472fa293323596a
vaibhgupta157 <vaibhgupta157@gmail.com>
2019-12-01 18:58:42 +0530

    changed in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,22 +2,20 @@
 
 
 echo $1 | sudo -S -k apt-get -y install gcc
-echo $1 | sudo -S -k apt-get -y install git
 echo $1 | sudo -S -k apt-get -y install python-dev
 echo $1 | sudo -S -k apt-get -y install python-pip
 echo $1 | sudo -S -k pip install netconf
 echo $1 | sudo -S -k pip install dicttoxml
 
-git clone https://github.com/robshakir/pyangbind.git
 
-cd pynagbind && python setup.py install 
+echo $1 | sudo -S -k python pyangbind/setup.py install 
 
 ssh-keygen -t rsa -b 2048 -f netconf-key -q -N ""
 
 
 echo "export NETCONF_DIR"=$(pwd) >> ~/.bashrc
 echo "NETCONF_DIR"=$(pwd) >> ~/.profile
-echo $1 | sudo -S -k echo "NETCONF_DIR"=$(pwd) >> /etc/environment
+echo "NETCONF_DIR"=$(pwd) | sudo tee -a /etc/environment
 source ~/.bashrc
 source ~/.profile
 echo "do you want to restart your computer to apply changes in /etc/environment file? yes(y)no(n)"
